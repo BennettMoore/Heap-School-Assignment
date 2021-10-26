755131f460834cd43a381fc282e012fecf0977fa
Bennett W Moore <bwm7637@cs.rit.edu>
2021-10-26 13:05:33 -0400

    Created empty HeapDT method

---
+++ HeapDT.c
@@ -0,0 +1,19 @@
+//
+// file : HeapDT.c
+//
+// @author Bennett Moore bwm7637@rit.edu
+//
+// // // // // // // // // // // // // //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "HeapDT.h"
+
+/*
+ * The main method
+ *
+ * @returns whether the program ran successfully or not
+ */
+int main(void){
+	return 0;
+}
