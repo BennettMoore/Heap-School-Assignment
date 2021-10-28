4616117ce63b275e1b5e4a0a6361f9403c17d086
Bennett Moore <bwm7637@rit.edu>
2021-10-27 21:46:38 -0400

    Added more rigorous testing, did not affect performance of HeapDT

--- test_HeapDT.c
+++ test_HeapDT.c
@@ -193,6 +193,85 @@ void test_char_heap( bool max_flag ) {
     return;
 }
 
+/// test_multi_heap tests having multiple Heaps active at a time, as well as very large heaps
+///
+/// @param max_flag is true if heap is a maxheap.
+void test_multi_heap(bool max_flag) {
+
+    printf( "test_multi_heap using %s ordering\n"
+          , (max_flag ? "max" : "min") );
+
+    char test_data_c[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+	    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    int test_data_i[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 
+	    37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52};
+    
+    printf( "character test data: [ " );
+    for ( size_t i = 0; i < sizeof( test_data_c ) / sizeof( char ); i++ ) {
+        printf( "%c ", test_data_c[i] );
+    }
+    printf( "]\n" );
+    
+    printf( "integer test data: [ " );
+    for ( size_t i = 0; i < sizeof( test_data_i ) / sizeof( int ); i++ ) {
+        printf( "%i ", test_data_i[i] );
+    }
+    printf( "]\n" );
+
+    Heap charHeap = NULL;
+    Heap intHeap = NULL;
+    if(max_flag){
+        charHeap = hdt_create(10, cmpcharmax, print_char);
+	intHeap = hdt_create(10, cmpintmax, print_int);
+    }
+    else{
+        charHeap = hdt_create(10, cmpcharmin, print_char);
+	intHeap = hdt_create(10, cmpintmin, print_int);
+    }
+    assert(charHeap);
+    assert(intHeap);
+    
+    for ( size_t i = 0; i < sizeof( test_data_c ) / sizeof( char ); i++ ) {
+        hdt_insert_item( charHeap, (void*)&test_data_c[i] );
+    }
+
+    for ( size_t i = 0; i < sizeof( test_data_i ) / sizeof( int ); i++ ) {
+        hdt_insert_item( intHeap, (void*)&test_data_i[i] );
+    }
+    printf( "Character heap size is now %zu\n", hdt_size( charHeap ) );
+    fprintf( stderr, "Character hdt_print to stderr:\n" );
+    hdt_print( charHeap, stderr );
+    
+    printf( "Emptying the character heap: " );
+    while ( hdt_size( charHeap ) ) {
+        char* vPointer = hdt_remove_top(charHeap);
+        char value = *vPointer;
+        free(vPointer);
+        printf( "%c ", value );
+    }
+    printf( "\n" );
+
+    hdt_destroy( charHeap );
+
+    
+    printf( "Integer heap size is now %zu\n", hdt_size( intHeap ) );
+    fprintf( stderr, "Integer hdt_print to stderr:\n" );
+    hdt_print( intHeap, stderr );
+
+    printf( "Emptying the integer heap: " );
+    while ( hdt_size( intHeap ) ) {
+        int* vPointer = hdt_remove_top(intHeap);
+        int value = *vPointer;
+        free(vPointer);
+        printf( "%i ", value );
+    }
+    printf( "\n" );
+
+    hdt_destroy( intHeap );
+
+    return;
+
+}
 
 /// main function runs tests using integer and char data payloads.
 /// The tests use both a minheap and a maxheap.
@@ -207,6 +286,9 @@ int main( void ) {
     test_char_heap( false );
     test_char_heap( true );
 
+    test_multi_heap( false );
+    test_multi_heap( true );
+    
     return EXIT_SUCCESS;
 }
 
